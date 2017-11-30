e3c1c5ce979113e043ed020a2d678665fb9412b0
pcs2216 <coronelsantiagop@gmail.com>
2017-11-30 15:50:40 -0600

    [REF] Agregar campos de etiqueta monedas y descripcion al modelo de productos y en us vista

--- atani_mod/models/products_inherit.py
+++ atani_mod/models/products_inherit.py
@@ -14,3 +14,15 @@ class x_Products(models.Model):
     )
     
     
+    x_etiqueta = fields.Selection(
+        string=u'Etiqueta Moneda',
+        help='Agregar etiqueta del tipo de moneda para filtrar y actualizarlos',
+        selection=[('MXN', 'MXN'), ('USD', 'USD')]
+    )
+    
+    x_descripcion = fields.Text(
+        string='Descripción',
+    )
+    
+    
+    
